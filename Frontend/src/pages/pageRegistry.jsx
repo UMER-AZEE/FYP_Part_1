75b508f2643b8f47fe4d13b960a2feb646e389e4
UMER-AZEE <51984@students.riphah.edu.pk>
2026-06-06 16:48:43 +0500

    Update Frontend/src/pages/pageRegistry.jsx

--- Frontend/src/pages/pageRegistry.jsx
+++ Frontend/src/pages/pageRegistry.jsx
@@ -1,8 +1,11 @@
 import ActivityLogPage from './ActivityLogPage'
 import CatalogPage from './CatalogPage'
+import IntegrationsPage from './IntegrationsPage'
+import InventoryPage from './InventoryPage'
 import InsightsPage from './InsightsPage'
 import LatencyPage from './LatencyPage'
 import PoliciesPage from './PoliciesPage'
+import PromptStudioPage from './PromptStudioPage'
 import SettingsPage from './SettingsPage'
 import SpendPage from './SpendPage'
 import UsersPage from './UsersPage'
@@ -10,7 +13,10 @@ import ViolationsPage from './ViolationsPage'
 
 export const pageRegistry = {
   insights: InsightsPage,
+  integrations: IntegrationsPage,
+  inventory: InventoryPage,
   policies: PoliciesPage,
+  'prompt-studio': PromptStudioPage,
   violations: ViolationsPage,
   users: UsersPage,
   'activity-log': ActivityLogPage,
